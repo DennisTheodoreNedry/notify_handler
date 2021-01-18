941f6136bb969b13c4c2fb4682d9304eb952a309
s9rA16Bf4 <s9rA16Bf4@gmail.com>
2021-01-18 20:11:55 +0100

    Update errorHandler.hpp

--- errorHandler.hpp
+++ errorHandler.hpp
@@ -1,10 +1,11 @@
 #ifndef ERRORHANDLER_HPP
+#define ERRORHANDLER_HPP 1
 
 /*MIT License
 
 Copyright (c) 2020 --///--//--
 
-/*Permission is hereby granted, free of charge, to any person obtaining a copy
+Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -20,7 +21,8 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.*/
+SOFTWARE.
+*/
 
 #include <string>
 #include <iostream>
